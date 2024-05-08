f82550711b99386ddff340e4947479071843abbb
AbdurRahman11072 <rjrahman014@gmail.com>
2024-05-09 02:58:09 +0600

    first

---
+++ app/dashboard/page.js
@@ -0,0 +1,16 @@
+
+import Card from '@/components/dashboard/card/card';
+
+
+const Dashboard = () => {
+  
+    return (
+        <div className="h-auto">
+            <div>
+               <Card />
+         </div>
+        </div>
+    );
+};
+
+export default Dashboard;
